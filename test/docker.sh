70ba945849c30a0f822f97a0811e7a654c5cfff7
Mark Lee <electronjs@lazymalevolence.com>
2018-01-30 18:38:12 -0800

    Missed a -y

--- test/docker.sh
+++ test/docker.sh
@@ -2,6 +2,6 @@
 
 cd /code
 
-apt install --no-install-recommends libavahi-compat-libdnssd-dev
+apt install -y --no-install-recommends libavahi-compat-libdnssd-dev
 npm install -g yarn
 DEBUG='electron-installer-snap:*' yarn make:snap:64
